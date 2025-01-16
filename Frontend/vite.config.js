9010f41290d9f6cea1032716b83c4dd3e60b8109
Dylan <holcombdylan12@gmail.com>
2025-01-15 23:26:44 -0600

    added vercellink

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'https://backend-nine-kappa-61.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
