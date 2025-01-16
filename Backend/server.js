e9b56bd74755af529397120a60e1cf940abad078
Dylan <holcombdylan12@gmail.com>
2025-01-15 21:48:00 -0600

    added vercel

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
-const app = require('./app');
+// const app = require('./app');
 
-app.listen(3000, function () {
-    console.log('App running on port 3000');
-});
+// app.listen(3000, function () {
+//     console.log('App running on port 3000');
+// });
